3fca641acd1943d38b17c7d698c5e295ef54b0f0
Mikel Gascon <mikelgascon@outlook.com>
2019-10-15 19:45:10 +0000

    made a function to find if two arrays are the same

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return true;
+
+  } else {
+    return false;
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  arraysAreEqual = false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (assertEqual(arr1[i], arr2[i])) {
+      arraysAreEqual = true;
+    } else {
+        arraysAreEqual = false;
+    }
+  }
+  return console.log(arraysAreEqual);
+};
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
--- head.js
+++ head.js
@@ -7,5 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const head = function(myArray) {
+  let arrHead = myArray[0];
+  return arrHead;
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
